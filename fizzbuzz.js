1a9e98a38df5ef34c7c24b694588c0667778cbe8
ayk0e <144247767+ayk0e@users.noreply.github.com>
2023-11-04 16:40:04 +0300

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,13 @@
+function fizzbuzz(n) {
+  for(let i = 1; i <= n; i++) {
+    let text = "";
+    
+    if(i % 3 === 0) text = text + "Fizz";
+    if(i % 5 === 0) text = text + "Buzz";
+    if(text === "") text = i;
+    
+    console.log(text);
+  }
+}
 
+fizzbuzz(100);
